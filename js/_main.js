3c7842259e57b3495c2fa1d46781c61509f8989a
mauriciogc <=mauriciogc@gmail.com>
2015-08-06 17:22:16 -0500

    Se optimiza el codigo

--- js/_main.js
+++ js/_main.js
@@ -1,63 +1,80 @@
-//Inicia
-function iniJs() {
-    'use strict';
-    /*global ajustaAlto,fnWayPoint,scrollTo, $*/
-    var w = window,
-        d = document,
-        wP = document.getElementsByClassName('waypoint'),
-        x;
+var globales = {
+    options: {
+        w: window,
+        d: document
+    },
+    init: function () {
+        'use strict';
+        /*global $*/
 
-    //Preloader
-    $('body').jpreLoader();
+        var t = this,
+            o = t.options;
 
-    $(w).on("resize", ajustaAlto);
-    $(d).on("ready", ajustaAlto);
+        $('body').jpreLoader();
 
-    //Parallax
-    if ($(w).width() > 480) {
-        $('.parallax-bg1').parallax("50%", 0.5);
-    }
+        $(o.w).on("resize", t.ajustarAlto);
+        $(o.d).on("ready", t.ajustarAlto);
 
-    //Animaciones
-    $('.animar').appear();
-    $(d.body).on('appear', '.animar', function () {
-        var an = $(this).attr("data-animation");
+        t.parallax();
+        t.animation();
+        t.header();
+        t.scroll();
+    },
+    parallax: function () {
+        'use strict';
+        var o = this.options;
 
-        if (an === "shake" || an === "tada" || an === "bounceIn") {
-            $(this).css('opacity', '1');
+        if ($(o.w).width() > 480) {
+            $('.parallax-bg1').parallax("50%", 0.5);
         }
-        if ($(w).width() > 480) {
-            $(this).each(function () {
-                $(this).addClass(an);
-            });
+    },
+    animation: function () {
+        'use strict';
+        var o = this.options;
+
+        $('.animar').appear();
+        $(o.d.body).on('appear', '.animar', function () {
+            var an = $(this).attr("data-animation");
+
+            if (an === "shake" || an === "tada" || an === "bounceIn") {
+                $(this).css('opacity', '1');
+            }
+            if ($(o.w).width() > 480) {
+                $(this).each(function () {
+                    $(this).addClass(an);
+                });
+            }
+        });
+    },
+    header: function () {
+        'use strict';
+        var t = this,
+            o = t.options,
+            x,
+            wP = document.getElementsByClassName('waypoint');
+
+        for (x = 0; x < wP.length; x += 1) {
+            t.fnWayPoint(x, wP);
         }
-    });
+    },
+    scroll: function () {
+        'use strict';
+        $('body').scrollTo(0);
+        $.scrollTo(0);
 
-    //Header
-    for (x = 0; x < wP.length; x += 1) {
-        fnWayPoint(x, wP);
-    }
+        $('.hwpMenu').click(function (evt) {
+            var dsID = this.getAttribute('data-scroll-id'),
+                dsClass = this.getAttribute('data-scroll-class');
+            if (dsID) {
+                $('body').scrollTo(document.getElementById(dsID), 800);
+            }
 
-
-    $('body').scrollTo(0);
-    $.scrollTo(0);
-
-    $('.hwpMenu').click(function (evt) {
-        var dscrollid = this.getAttribute('data-scroll-id'),
-            dscrollclass = this.getAttribute('data-scroll-class');
-        if (dscrollid) {
-            $('body').scrollTo(document.getElementById(dscrollid), 800);
-        }
-
-        if (dscrollclass) {
-            $('body').scrollTo(document.getElementsByClassName(dscrollclass)[0], 800);
-        }
-    });
-}
-
-
-//ajustaAlto
-var ajustaAlto = function () {
+            if (dsClass) {
+                $('body').scrollTo(document.getElementsByClassName(dsClass)[0], 800);
+            }
+        });
+    },
+    ajustarAlto: function () {
         'use strict';
 
         var dA = $('.domAlto'),
@@ -89,7 +106,7 @@ var ajustaAlto = function () {
             "margin-top": tM + "px"
         });
     },
-    fnWayPoint = function (x, wayPoints) {
+    fnWayPoint: function (x, wayPoints) {
         "use strict";
         /*global ajustaAlto, Waypoint, $*/
         var headWayPoint = document.getElementById('headerWayPoint');
@@ -107,17 +124,13 @@ var ajustaAlto = function () {
                 }
             }
         });
-    };
+    }
+};
 
 //Document Ready
 $(document).ready(function () {
     "use strict";
-    /* global iniJs */
-    iniJs();
+    /* global globales,window */
+    globales.init();
 });
 
-/*property
-    addClass, appear, attr, body, className, click, css, each, element,
-    getAttribute, getElementById, getElementsByClassName, handler, height,
-    jpreLoader, length, offset, on, parallax, ready, removeClass, width, console
-*/
