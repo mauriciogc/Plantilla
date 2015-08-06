d616a07bb75a6fc466578de132d002a8d245fc8a
mauriciogc <=mauriciogc@gmail.com>
2015-08-06 13:22:51 -0500

    Se hace la validacion con el JSLint y se mete el plugin de scrollTo

--- js/_main.js
+++ js/_main.js
@@ -1,7 +1,7 @@
 //Inicia
 function iniJs() {
     'use strict';
-    /*global ajustaAlto,fnWayPoint, $*/
+    /*global ajustaAlto,fnWayPoint,scrollTo, $*/
     var w = window,
         d = document,
         wP = document.getElementsByClassName('waypoint'),
@@ -37,41 +37,58 @@ function iniJs() {
     for (x = 0; x < wP.length; x += 1) {
         fnWayPoint(x, wP);
     }
+
+
+    $('body').scrollTo(0);
+    $.scrollTo(0);
+
+    $('.hwpMenu').click(function (evt) {
+        var dscrollid = this.getAttribute('data-scroll-id'),
+            dscrollclass = this.getAttribute('data-scroll-class');
+        if (dscrollid) {
+            $('body').scrollTo(document.getElementById(dscrollid), 800);
+        }
+
+        if (dscrollclass) {
+            $('body').scrollTo(document.getElementsByClassName(dscrollclass)[0], 800);
+        }
+    });
 }
+
 
 //ajustaAlto
 var ajustaAlto = function () {
-    'use strict';
+        'use strict';
 
-    var dA = $('.domAlto'),
-        ww = $(window).width(),
-        wh = $(window).height(),
-        dvC = $('.domAlto .vcenter'),
-        daC = $('.domAlto .content'),
-        cH,
-        pH,
-        tM;
+        var dA = $('.domAlto'),
+            ww = $(window).width(),
+            wh = $(window).height(),
+            dvC = $('.domAlto .vcenter'),
+            daC = $('.domAlto .content'),
+            cH,
+            pH,
+            tM;
 
-    dA.css({
-        'min-height': wh + 'px'
-    });
+        dA.css({
+            'min-height': wh + 'px'
+        });
 
-    cH = dvC.height();
-    pH = dA.height();
-    tM = (pH - cH) / 2;
+        cH = dvC.height();
+        pH = dA.height();
+        tM = (pH - cH) / 2;
 
-    dvC.css({
-        "margin-top": tM + "px"
-    });
+        dvC.css({
+            "margin-top": tM + "px"
+        });
 
-    cH = daC.height();
-    pH = dA.height();
-    tM = (pH - cH) / 2;
+        cH = daC.height();
+        pH = dA.height();
+        tM = (pH - cH) / 2;
 
-    daC.css({
-        "margin-top": tM + "px"
-    });
-},
+        daC.css({
+            "margin-top": tM + "px"
+        });
+    },
     fnWayPoint = function (x, wayPoints) {
         "use strict";
         /*global ajustaAlto, Waypoint, $*/
@@ -102,5 +119,5 @@ $(document).ready(function () {
 /*property
     addClass, appear, attr, body, className, click, css, each, element,
     getAttribute, getElementById, getElementsByClassName, handler, height,
-    jpreLoader, length, offset, on, parallax, ready, removeClass, width
+    jpreLoader, length, offset, on, parallax, ready, removeClass, width, console
 */
